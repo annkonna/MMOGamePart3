f36083d90a4cf22158ebe7f0927d0d4f54118e76
agkon <annkonna@buffalo.edu>
2019-05-04 22:38:04 -0400

    Added multi-player capability.

--- src/WebApp/welcome.js
+++ src/WebApp/welcome.js
@@ -1,9 +1,4 @@
-setInterval(updatePlayers, 3000);
-
-function updatePlayers() {
-  document.getElementById("players").value = "Number of players: "+Math.floor((Math.random() * 100) + 1);
-}
-
 function redirect() {
-  window.location.href="game.html"
+	localStorage.setItem("name", document.getElementById("name").value);
+	window.location.href="game.html"
 }
